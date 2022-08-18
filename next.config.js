17d9ca0cc8f23436a7108c3818c3c249e6593237
liam-earle <38230039+liam-earle@users.noreply.github.com>
2022-08-17 23:11:19 -0230

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/friends-of-victoria-park.github.io/' : ''
+  assetPrefix: isProd ? 'friends-of-victoria-park.github.io/' : ''
 }
