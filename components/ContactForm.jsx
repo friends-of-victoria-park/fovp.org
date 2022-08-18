be35ac4bd98e7d3123e04051a4e1698ec8c106ee
liam-earle <38230039+liam-earle@users.noreply.github.com>
2022-08-18 21:26:10 -0230

    Contact Form Changes

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -21,6 +21,14 @@ export function ContactForm(props) {
 					</div>
 				</div>
 				<div className="flex flex-wrap -mx-3 mb-6">
+					<div className="w-full px-3 mb-6 md:mb-0">
+						<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+							Subject
+						</label>
+						<input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-full-name" type="text" name="subject" required />
+					</div>
+				</div>
+				<div className="flex flex-wrap -mx-3 mb-6">
 					<div className="w-full px-3">
 						<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
 							Message
