c38d5def379dc6520adf0661d42fe35f21041fba
liam-earle <38230039+liam-earle@users.noreply.github.com>
2022-08-18 22:13:40 -0230

    Open Lantern Fest in new tab

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -58,7 +58,7 @@ export const Navbar = () => {
 							</a>
 						</Link>
 						<Link href='https://lanternfest.ca/'>
-							<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-blue-600 hover:text-white'>
+							<a target='_blank' className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-blue-600 hover:text-white'>
 								Lantern Festival
 							</a>
 						</Link>
